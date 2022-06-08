cd8a382c5af95eb1f5ae318ee246a4634df96766
kbaginski <106392277+kAArter33@users.noreply.github.com>
2022-06-08 16:38:26 +0200

    poprawka 3

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ let a2 = parseInt(prompt("Wprowadź liczbe drugą"));
 
 function dzialanie(num1,num2) {
 
-    let sum = a1 + a2;
-    let sub = a1 - a2;
-    let mult = a1 * a2;
+    let sum = num1 + num2;
+    let sub = num1 - num2;
+    let mult = num1 * num2;
 
     if (sum < 0 || sub < 0 || mult < 0) {
         console.log("Wynik nieprawidłowy")
